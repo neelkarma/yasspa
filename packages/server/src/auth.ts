4ea98ebc98257b705810fcdcb0ff54941bdfec2f
chickensalt <nsdj.sharma@gmail.com>
2021-03-27 14:17:21 +1100

    idk why git didnt recognise these but ye

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,53 +1,75 @@
 import { AuthorizationCode as OAuth2Client } from "simple-oauth2";
 import type { Express } from "express";
 
-export default (app: Express, host: string, clientId: string, clientSecret: string) => {
+export default (app: Express) => {
   const client = new OAuth2Client({
     client: {
       id: process.env.CLIENT_ID!,
-      secret: process.env.CLIENT_SECRET!
+      secret: process.env.CLIENT_SECRET!,
     },
     auth: {
-      tokenHost: "https://student.sbhs.net.au/api"
-    }
+      tokenHost: "https://student.sbhs.net.au/api",
+      tokenPath: "/token",
+      authorizePath: "/authorize",
+    },
   });
 
   const authorizationUri = client.authorizeURL({
     redirect_uri: process.env.REDIRECT_URI!,
-    scope: "all-ro"
+    scope: "all-ro",
   });
 
+  // Login
   app.get("/auth/login", (req, res) => {
     res.redirect(authorizationUri);
   });
 
-  app.get("/auth/logout", (req, res) => {
+  // Logout
+  app.get("/auth/logout", async (req, res) => {
     req.session.destroy(() => {
       res.redirect("/");
     });
   });
 
+  // Login Callback - Storing token in session
   app.get("/auth/callback", async (req, res) => {
     const { code } = req.query;
     try {
-      const tokenObject = await client.getToken({
+      req.session.token = await client.getToken({
         code: code!.toString(),
-        redirect_uri: process.env.REDIRECT_URI!
+        redirect_uri: process.env.REDIRECT_URI!,
       });
-      req.session.token = tokenObject
-      res.redirect("/")
-    } catch (e) {
+      res.redirect("/");
+    } catch {
       return res.status(500).json({ error: "The SBHS server fucked up" });
     }
   });
 
-  app.get("/auth/getToken", (req, res) => {
-    if (!req.session.token) return res.status(403).json({ error: "Invalid Authorization" });
-    if (Date.now() < req.session.token.expires) return res.json({
-      accessToken: req.session.token.accessToken,
-      expires: req.session.token.expires
-    });
+  // Getting token from session
+  app.get("/auth/getToken", async (req, res) => {
+    if (!req.session.token)
+      return res.status(403).json({ error: "Invalid Authorization" });
 
-    client.createToken(req.session.token)
-  })
-}
+    const accessToken = client.createToken(req.session.token);
+
+    if (!accessToken.expired())
+      return res.status(200).json({
+        accessToken: req.session.token.access_token,
+        expires: req.session.token.expires_in,
+      });
+
+    // Refresh the token
+    try {
+      req.session.token = await accessToken
+        .refresh()
+        .then((tokenObject) => tokenObject.token);
+
+      res.status(200).json({
+        accessToken: req.session.token!.access_token,
+        expires: req.session.token!.expires_in,
+      });
+    } catch {
+      return res.status(500).json({ error: "The SBHS server fucked up" });
+    }
+  });
+};
