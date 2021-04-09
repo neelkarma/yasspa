1876df4060133b888871097ca62d05dc9bdfce9b
chickensalt <nsdj.sharma@gmail.com>
2021-04-09 11:17:14 +1000

    feat: landing page and new fonts

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,16 +1,9 @@
+import "@fontsource/inter";
+import "@fontsource/raleway";
 import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { SWRConfig } from "swr";
+import Landing from "./landing";
 
 export const App = () => (
   <SWRConfig
@@ -18,26 +11,15 @@ export const App = () => (
       fetcher: (...args) => fetch(args).then((res) => res.json()),
     }}
   >
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+    <ChakraProvider
+      theme={extendTheme({
+        fonts: {
+          body: "Inter",
+          heading: "Raleway",
+        },
+      })}
+    >
+      <Landing />
     </ChakraProvider>
   </SWRConfig>
 );
