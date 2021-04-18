f48391e06188d47a2a42ccd5f9ce06c15038b4c0
chickensalt <59462098+neelkarma@users.noreply.github.com>
2021-04-18 18:30:06 +1000

    format everything

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -6,23 +6,21 @@ import Loading from "./loading";
 import theme from "./theme";
 
 const App: React.FC<{}> = () => {
-	const { data } = useSWR("/auth/status");
-	useEffect(() => console.log(data), [data]);
-	return (
-		<SWRConfig
-			value={{ fetcher: (...args) => fetch(args).then((res) => res.json()) }}
-		>
-			<ChakraProvider
-				theme={theme}
-			>
-				{(() => {
-					if (!data) return <Loading />;
-					if (!data.authenticated) return <Landing />
-					return <Text>TODO: Implement this</Text>;
-				})()}
-			</ChakraProvider>
-		</SWRConfig>
-	);
+  const { data } = useSWR("/auth/status");
+  useEffect(() => console.log(data), [data]);
+  return (
+    <SWRConfig
+      value={{ fetcher: (...args) => fetch(args).then((res) => res.json()) }}
+    >
+      <ChakraProvider theme={theme}>
+        {(() => {
+          if (!data) return <Loading />;
+          if (!data.authenticated) return <Landing />;
+          return <Text>TODO: Implement this</Text>;
+        })()}
+      </ChakraProvider>
+    </SWRConfig>
+  );
 };
 
 export default App;
--- packages/app/src/theme.ts
+++ packages/app/src/theme.ts
@@ -4,22 +4,22 @@ import "@fontsource/raleway";
 import { extendTheme } from "@chakra-ui/react";
 
 export default extendTheme({
-	config: {
-		fonts: {
-			body: "Inter",
-			heading: "Raleway",
-		},
-		components: {
-			Button: {
-				variants: {
-					primary: {
-						bg: "blue.400",
-						color: "white",
-					},
-				},
-			},
-		},
-		initialColorMode: "dark",
-		useSystemColorMode: true,
-	},
+  config: {
+    fonts: {
+      body: "Inter",
+      heading: "Raleway",
+    },
+    components: {
+      Button: {
+        variants: {
+          primary: {
+            bg: "blue.400",
+            color: "white",
+          },
+        },
+      },
+    },
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
 });
