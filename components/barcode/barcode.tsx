12c973054c1342516f96cd665a04cf106274e62a
chickensalt <nsdj.sharma@gmail.com>
2022-01-05 13:24:37 +1100

    add preliminary barcode feature

--- components/barcode/barcode.tsx
+++ components/barcode/barcode.tsx
@@ -1,12 +1,17 @@
-import { Text, Heading, Spinner } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 import { CenterCard, Card } from "components/card";
 import { SettingsContext } from "components/settingscontext";
-import { useAwardScheme, useUserInfo } from "lib/clientFetchResources";
-import { useContext } from "react";
+import JsBarcode from "jsbarcode";
+import { useUserInfo } from "lib/clientFetchResources";
+import { useContext, useEffect } from "react";
 
 export const Barcode: React.FC<{}> = () => {
   const { res } = useUserInfo();
   const { debug } = useContext(SettingsContext);
+
+  useEffect(() => {
+    if (res) JsBarcode("#barcode", res.data.studentId);
+  }, [res]);
 
   if (!res)
     return (
@@ -17,5 +22,10 @@ export const Barcode: React.FC<{}> = () => {
 
   if (debug) console.log("profile user data", res);
 
-  return <Card>{res.data.studentId}</Card>;
+  return (
+    <Card>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img id="barcode" alt={`Student ID Barcode for ${res.data.studentId}`} />
+    </Card>
+  );
 };
