abe2ce523e5350bd985b05881905c0e7f1a33225
Neel Sharma <nsdj.sharma@gmail.com>
2023-12-10 22:47:21 +1100

    another minor design tweak

--- src/lib/components/PeriodContainer.svelte
+++ src/lib/components/PeriodContainer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="flex items-center gap-3 px-3 py-2 rounded-lg border-stone-600 border-[1px]"
+  class="flex items-center gap-3 px-3 py-2 rounded-lg border-stone-700 border-[1px]"
   class:text-stone-500={isCancelled}
 >
   <span class="text-lg md:text-xl">{period}</span>
